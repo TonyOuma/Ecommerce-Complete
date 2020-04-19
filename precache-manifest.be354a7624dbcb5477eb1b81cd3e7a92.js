2d72773c20641003f3299bf3eec2066c483244bb
TonyOuma <kyrietony@gmail.com>
2020-04-19 17:25:07 +0300

    Updates

---
+++ precache-manifest.be354a7624dbcb5477eb1b81cd3e7a92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48ad02a239ac2731a0595fdad120ae2",
+    "url": "/Ecommerce-Complete/index.html"
+  },
+  {
+    "revision": "c896440c21071b4c30c0",
+    "url": "/Ecommerce-Complete/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "38c22153c14f42203821",
+    "url": "/Ecommerce-Complete/static/css/main.b5ab1ff1.chunk.css"
+  },
+  {
+    "revision": "c896440c21071b4c30c0",
+    "url": "/Ecommerce-Complete/static/js/2.acacec60.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Ecommerce-Complete/static/js/2.acacec60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c22153c14f42203821",
+    "url": "/Ecommerce-Complete/static/js/main.5a270101.chunk.js"
+  },
+  {
+    "revision": "642bf7c8d07e7cc4cb8c",
+    "url": "/Ecommerce-Complete/static/js/runtime-main.31391845.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Ecommerce-Complete/static/media/logo.9f7e506d.svg"
+  }
+]);
